aa396bdd1b52f0ae46db485276c10f8664ae5556
Ishmael Sibisi <118228192+IshmaelBash@users.noreply.github.com>
2023-05-25 05:02:51 +0200

    Update SampleDataController.cs

--- {.Project.Name}/Controllers/SampleDataController.cs
+++ {.Project.Name}/Controllers/SampleDataController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace butler{ .Project.Name }.Controllers
+namespace bb{ .Project.Name }.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
