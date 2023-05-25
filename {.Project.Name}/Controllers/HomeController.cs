e5118c427ea3a5cfe52857a3b777ecacb903a43e
Ishmael Sibisi <118228192+IshmaelBash@users.noreply.github.com>
2023-05-25 05:03:02 +0200

    Update HomeController.cs

--- {.Project.Name}/Controllers/HomeController.cs
+++ {.Project.Name}/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace butler{ .Project.Name }.Controllers
+namespace bb{ .Project.Name }.Controllers
 {
     public class HomeController : Controller
     {
