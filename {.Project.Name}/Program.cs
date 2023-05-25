21a72dbc387bf25f2855b5f09a98667e52df4347
Ishmael Sibisi <118228192+IshmaelBash@users.noreply.github.com>
2023-05-25 05:01:01 +0200

    Update Program.cs

--- {.Project.Name}/Program.cs
+++ {.Project.Name}/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace butler{ .Project.Name }
+namespace bb{ .Project.Name }
 {
     public class Program
     {
